2d2b3b7e2171bbf1bf19c2c25dfd30d12050337f
zhy <zhy1997@mail.dlut.edu.cn>
2019-11-05 16:23:13 +0800

    修改日期：2019.11.05 最后完成：1.1.13

--- src/cn/dlut/zhy/unit01/Test1_1_13.java
+++ src/cn/dlut/zhy/unit01/Test1_1_13.java
@@ -1,4 +1,15 @@
 package cn.dlut.zhy.unit01;
 
 public class Test1_1_13 {
+    public static void main(String[] args) {
+        int[][] array={{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
+        int M=array.length;
+        int N=array[0].length;
+        for(int i=0;i<N;i++){
+            for(int j=0;j<M;j++){
+                System.out.print(array[j][i]+"\t");
+            }
+            System.out.println("");
+        }
+    }
 }
